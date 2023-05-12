e283dadf354056fe7f1de7b9896043b32ab2b939
Nikhil <nikhilshah.net@hotmail.com>
2023-05-12 11:50:48 +0530

    This is initial project commit for github

---
+++ shoppingAPP - VS/shoppingAPP - VS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shoppingAPP___VS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
